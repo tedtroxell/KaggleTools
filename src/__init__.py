f00d0d687c292f78a01ea700471353b6e1086863
tedtroxell <theador.t@gmail.com>
2021-02-25 17:34:01 -0500

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-class BaseTool:
-
-    def _test_fit(self):raise NotImplementedError('"_test_fit()" has not been implemented')
-    def fit(self,X,y):raise NotImplementedError('"fit(X,y)" has not been implemented')
-    def predict(self,X):raise NotImplementedError('"predict(X)" has not been implemented')
